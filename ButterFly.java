0c641b0a6e368c02c35be1cfe5c2204759650db3
Karan Kumar <121441284+karansingh1710@users.noreply.github.com>
2023-04-22 11:31:31 +0530

    Add files via upload

---
+++ ButterFly.java
@@ -0,0 +1,16 @@
+public class ButterFly {
+    public static void main(String[] args) {
+        int n=7;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i<=j) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
